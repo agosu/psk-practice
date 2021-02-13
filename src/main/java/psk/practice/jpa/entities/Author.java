2d312727d215e338c60c8dfcfb0685202b82d311
Agota <45971426+agosu@users.noreply.github.com>
2021-02-13 20:36:31 +0200

    Removed not needed files for jpa code first

--- src/main/java/psk/practice/jpa/entities/Author.java
+++ src/main/java/psk/practice/jpa/entities/Author.java
@@ -1,4 +1,26 @@
 package psk.practice.jpa.entities;
 
-public class Author {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter
+public class Author implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 25)
+    private String firstname;
+
+    @Size(max = 25)
+    private String lastname;
 }
--- src/main/java/psk/practice/jpa/entities/Character.java
+++ src/main/java/psk/practice/jpa/entities/Character.java
@@ -1,4 +1,25 @@
 package psk.practice.jpa.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter @Setter
 public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 25)
+    private String firstname;
+
+    @Size(max = 25)
+    private String lastname;
+
+    @ManyToOne
+    private Book book;
 }
